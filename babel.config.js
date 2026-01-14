3973be07d8432fab0ff9f9fb4d342be4a4a2ad35
Benjamin Pettinari <bentennis@hotmail.com>
2026-01-14 12:09:56 -0600

    chore: update babel.config.js presets/plugins per request

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,12 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['module:metro-react-native-babel-preset', '@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-    plugins: [
-      '@babel/plugin-transform-runtime',
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-transform-modules-commonjs',
-      '@babel/plugin-transform-async-to-generator',
-      '@babel/plugin-transform-template-literals',
-      '@babel/plugin-transform-arrow-functions',
-      '@babel/plugin-syntax-jsx', // Extra for JSX errors
-    ],
-  };
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: 'node', useBuiltIns: 'use-dynamic-import' }],
+    ['@babel/preset-typescript'],
+  ],
+  plugins: [
+    ['@babel/plugin-transform-runtime'],
+    ['@babel/plugin-proposal-class-properties'],
+    ['@babel/plugin-proposal-json-stringify']
+  ],
+  sourceFileName: "babel.config.js",
 };
